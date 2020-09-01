a2ddc77ef1e81f3918264f1d59e34db5552e1fb7
Johannes Schmidt <mail@johannez.com>
2020-09-01 12:13:01 -0700

    Refactored migrations

--- src/Migrations/Migration.php
+++ src/Migrations/Migration.php
@@ -69,6 +69,33 @@ abstract class Migration
     }
 
     /**
+    * Load CSV file into an array
+    *
+    * @param file_path System path to CSV file.
+    *
+    * @return array CSV data.
+    */
+    public function loadCsvFile($file_path)
+    {
+        $data = [];
+
+        if (file_exists($file_path)) {
+            $data = array_map('str_getcsv', file($file_path));
+
+            array_walk($data, function(&$row) use ($data) {
+                $row = array_combine($data[0], $row);
+            });
+
+            array_shift($data);
+        }
+        else {
+            \WP_CLI::error("Import file ($file_path) does not exist.");
+        }
+
+        return $data;
+    }
+
+    /**
      * Import an image into Wordpress.
      * This assume that all images are in an import folder. And then
      * filename is then used to copy the file over to the wp_uploads folder
