67ce5729a4bfbff78aae762e07323b4e07b9534d
Johannes Schmidt <mail@johannez.com>
2020-09-01 12:15:42 -0700

    Improved tailwind settings

--- theme/tailwind.config.js
+++ theme/tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   theme: {
     extend: {
       maxWidth: {
-        
+        'container': '100rem', // 1600px
       },
       colors: {
       
@@ -16,6 +16,9 @@ module.exports = {
       boxShadow: {
         'thick': '2px 2px 10px 3px rgba(0,0,0,0.4)'
       }
+    },
+    fontFamily: {
+      'body': ['Noto Sans', 'Helvetica Neue', 'sans-serif']
     }
   },
   variants: {
